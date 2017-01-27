ca1c41cfef386b1816c36cc7c83beb0d94fe9691
Max Svistunov <max.svistunov@gmail.com>
2017-01-27 14:41:32 +0200

    config.php

---
+++ src/config.php
@@ -0,0 +1,26 @@
+<?php 
+define("ABS", __DIR__);
+define("LANGUAGES", "en,ru,de");
+define("DB_HOST", "127.0.0.1");
+define("DB_LOGIN", "msv");
+define("DB_PASSWORD", "");
+define("DB_NAME", "msv");
+define("DB_REQUIRED", false);
+define("DATE_FORMAT", "d.m.Y H:i:s");
+define("PROTOCOL", "http://");
+define("MASTERHOST", "");
+define("UPLOAD_FILES_PATH", __DIR__."/content");
+define("CONTENT_URL", "/content");
+define("PHP_HIDE_ERRORS", false);
+define("DEBUG", true);
+define("DEBUG_LOG", "/var/log/msv-debug.log");
+define("SITE_CLOSED", false);
+define("SHOW_ADMIN_MENU", 1);
+define("PHP_LOCALE", "en_US.UTF-8");
+define("PHP_TIMEZONE", "Europe/Kiev");
+define("DATABATE_ENCODING", "utf8");
+define("FORSE_TRAILING_SLASH", true);
+define("SUBDOMAIN_LANGUAGES", true);
+define("REP", "http://rep.msvhost.com/rep/main/");
+define("USER_HASH_PASSWORD", true);
+define("USER_IGNORE_PRIVILEGES", false);
