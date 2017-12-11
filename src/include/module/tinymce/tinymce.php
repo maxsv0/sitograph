f5dff1af82d0cdea09274f98a49da273da02d5ef
Max Svistunov <max.svistunov@gmail.com>
2017-12-11 09:43:33 +0100

    toolbar/plugins
    #74

--- src/include/module/tinymce/tinymce.php
+++ src/include/module/tinymce/tinymce.php
@@ -18,16 +18,19 @@ $(document).ready(function() {
 	tinymce.init({
 		selector: '.editor',
 		language: '".LANG."',
-		height: 250,
+		height: 350,
 		verify_html : false,
 		convert_urls : false,
 		menubar: false,
+		toolbar_items_size: 'small',
 		plugins: [
-				'advlist autolink lists link image charmap print preview anchor',
-				'searchreplace visualblocks code fullscreen',
-				'insertdatetime media table contextmenu paste code'
+				'advlist autolink autosave lists link image charmap print preview anchor pagebreak hr',
+				'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+				'table contextmenu directionality emoticons textcolor paste fullpage textcolor colorpicker textpattern'
 		],
-	toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image code',
+		toolbar1: 'undo redo | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect',
+        toolbar2: 'cut copy paste | searchreplace | bullist numlist | outdent indent blockquote | link unlink anchor image media code | insertdatetime preview | forecolor backcolor',
+        toolbar3: 'table | hr removeformat | subscript superscript | charmap emoticons | print fullscreen | ltr rtl | visualchars visualblocks nonbreaking pagebreak restoredraft',
 	  content_css: [
 		$cssList
 	  ]
