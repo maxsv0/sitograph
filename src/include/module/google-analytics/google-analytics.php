f430e965d89ed48a23295ed0bab1ec8680ae2ada
maxsv0 <max.svistunov@gmail.com>
2018-04-11 10:34:48 +0200

    Include analytics.js locally
    fix #133

--- src/include/module/google-analytics/google-analytics.php
+++ src/include/module/google-analytics/google-analytics.php
@@ -7,7 +7,7 @@ if ($code) {
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	  })(window,document,'script','/content/js/analytics.js','ga');
 	  ";
 	$includeCode .= "ga('create', '$code', 'auto');\n";
 	$rowUser = msv_get("website.user");
