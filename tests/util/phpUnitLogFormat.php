1be62e8d9bc29bd38e62a7f939c2a05a982de0ce
maxsv0 <max.svistunov@gmail.com>
2018-05-02 19:19:03 +0200

    phpUnitLogFormat for better reading

--- tests/util/phpUnitLogFormat.php
+++ tests/util/phpUnitLogFormat.php
@@ -7,6 +7,7 @@ if (empty($logFile)) die();
 if (empty($reportFile)) die();
 
 $cont = file_get_contents($logFile);
+echo "File found: $logFile ".strlen($cont)." bytes\n";
 
 $a = strpos($cont, "</header>");
 $cont = substr($cont, $a+9);
@@ -19,4 +20,29 @@ $cont = $cont."</div>";
 $cont = preg_replace('#<a.*?>(.*?)</a>#i', '\1', $cont);
 $cont = preg_replace('#<span.*?>(.*?)</span>#i', '', $cont);
 
-file_put_contents($reportFile, $cont);
+$tr_list = explode("<tr>", $cont);
+echo count($tr_list)." lines found\n";
+
+$cont_filtered = "";
+$i = 0;
+foreach ($tr_list as $block) {
+    // skip empty lines
+    if (strpos($block, "class=\"\">") !== false) continue;
+    // skip title
+    if (strpos($block, "Code Coverage") !== false) continue;
+
+    if (strpos($block, ">Total<") !== false) {
+        $tr_text = "<tr class='lead'>";
+    } else {
+        $tr_text = "<tr>";
+    }
+    if ($i++>0) {
+        $cont_filtered .= $tr_text;
+    }
+
+    $cont_filtered .= $block;
+}
+
+echo "Write to file $reportFile ".strlen($cont_filtered)." bytes\n";
+
+file_put_contents($reportFile, $cont_filtered);
