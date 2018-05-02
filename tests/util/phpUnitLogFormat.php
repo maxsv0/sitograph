b58a4810b623ac0e2497bdae8618836ef64cec6b
maxsv0 <max.svistunov@gmail.com>
2018-05-02 19:43:44 +0200

    phpUnitLogFormat nice table

--- tests/util/phpUnitLogFormat.php
+++ tests/util/phpUnitLogFormat.php
@@ -26,8 +26,6 @@ echo count($tr_list)." lines found\n";
 $cont_filtered = "";
 $i = 0;
 foreach ($tr_list as $block) {
-    // skip empty lines
-    if (strpos($block, "class=\"\">") !== false) continue;
     // skip title
     if (strpos($block, "Code Coverage") !== false) continue;
 
@@ -39,6 +37,17 @@ foreach ($tr_list as $block) {
     if ($i++>0) {
         $cont_filtered .= $tr_text;
     }
+
+    $td_list = explode("<td", $block);
+
+    if (count($td_list) === 5) {
+    	unset($td_list[4]);
+	} elseif (count($td_list) === 11) {
+    	unset($td_list[8]);
+    	unset($td_list[9]);
+    	unset($td_list[10]);
+	}
+	$block = implode("<td", $td_list);
 
     $cont_filtered .= $block;
 }
